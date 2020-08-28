40bfc40a8fe309b411f16bc7e10f5eef224a3016
Danilo Sambugaro <dan.sambugaro@gmail.com>
2020-08-28 16:44:20 -0300

    update protobuf

--- 04/client/request.py
+++ 04/client/request.py
@@ -2,30 +2,36 @@ class Request:
     """Class representing a request"""
     
     action = None  # 1 - set, 2 - remove or 3 - query
-    academic_code = None  # students ids
+    target = None  # target of action
+    academic_code = None  # student id
     course_code = None  # course id
     academic_year = None  # academic year
     academic_semester = None  # academic semester
-    grade = None  # grade to set
-    absences = None  # absences to set
+    value = None  # value to set
 
-    def __init__(self, action=3,
+    def __init__(self, action=None, target=None,
                  academic_code=None, course_code=None,
                  academic_year=None, academic_semester=None,
-                 grade=None, absences=None):
+                 value=None):
         self.action = action
+        self.target = target
         self.academic_code = academic_code
         self.course_code = course_code
         self.academic_year = academic_year
         self.academic_semester = academic_semester
-        self.grade = grade
-        self.absences = absences
+        self.value = value
 
     def get_action(self):
         return self.action
 
     def set_action(self, value):
         self.action = value
+    
+    def get_target(self):
+        return self.target
+
+    def set_target(self, value):
+        self.target = value
     
     def get_academic_code(self):
         return self.academic_code
@@ -51,14 +57,8 @@ class Request:
     def set_academic_semester(self, value):
         self.academic_semester = value
     
-    def get_grade(self):
-        return self.grade
+    def get_value(self):
+        return self.value
 
-    def set_grade(self, value):
-        self.grade = value
-    
-    def get_absences(self):
-        return self.absences
-
-    def set_absences(self, value):
-        self.absences = value
+    def set_value(self, value):
+        self.value = value
